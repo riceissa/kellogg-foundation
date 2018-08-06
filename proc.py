4fababefec5bb82c6c767f2fcc22a6de126860af
Issa Rice <riceissa@gmail.com>
2018-08-06 13:48:49 -0700

    more progress

--- proc.py
+++ proc.py
@@ -31,13 +31,17 @@ def main():
     # save) this will catch it; (2) the ordering from glob is not in numerical
     # order.
     for page in range(1, last_page + 1):
+        print(f"Doing page {page}", file=sys.stderr)
         with open(sys.argv[1] + f"/page-{page}.html", "r") as f:
             soup = BeautifulSoup(f, "lxml")
-            print_sql(soup_to_grants(soup))
+            print_sql(soup_to_grants(soup, page, last_page))
 
 
-def soup_to_grants(soup):
-    for result in soup.find_all("section", {"class": "grantResult"}):
+def soup_to_grants(soup, page, last_page):
+    grant_result = soup.find_all("section", {"class": "grantResult"})
+    # Make sure we have a full page of grants
+    assert page == last_page or len(grant_result) == 100
+    for result in grant_result:
         grantee = result.find("h1").text.strip()
         url = result.find("h1").find("a")["href"]
 
@@ -52,15 +56,23 @@ def soup_to_grants(soup):
         dollar_loc = date_amount.index("$")
         date_range = date_amount[:dollar_loc].strip()
         start_date = date_range.split(" - ")[0]
-        # For some reason, the months appear both in the full form and in
-        # abbreviated form
-        try:
-            donation_date = datetime.datetime.strptime(start_date, "%b. %d, %Y")
-        except ValueError:
-            donation_date = datetime.datetime.strptime(start_date, "%B %d, %Y")
+        if date_range:
+            # For some reason, the months appear both in the full form and in
+            # abbreviated form
+            try:
+                donation_date = datetime.datetime.strptime(start_date, "%b. %d, %Y")
+            except ValueError:
+                donation_date = datetime.datetime.strptime(start_date, "%B %d, %Y")
+            donation_date = donation_date.strftime("%Y-%m-%d")
+        else:
+            donation_date = ""
         amount = date_amount[dollar_loc:].strip().replace("$", "").replace(",", "")
 
-        location = result.find("div", {"class": "geo-focus"}).text.strip()
+        try:
+            location = result.find("div", {"class": "geo-focus"}).text.strip()
+        except AttributeError:
+            location = ""
+
         yield {
                 "grantee": grantee,
                 "url": url,
